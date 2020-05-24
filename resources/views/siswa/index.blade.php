697f86ab005909ed5d3be94a7d39c44cd64139d2
amaliakirfani <Irfaniamalia6869@gmail.com>
2020-05-24 20:10:02 +0700

    update laravel

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -1,50 +1,56 @@
 @extends('layouts.master')
 
 @section('content')
-            @if(session('sukses'))
-            <div class="alert alert-success" role="alert">
-                {{session('sukses')}}
-            </div>
-            @endif
 
+<div class="main">
+    <div class="main-content">
+        <div class="container-fluid">
             <div class="row">
-                <div class="col-6">
-                    <h1>DATA SISWA</h1>
+                <div class="col-md-12">
+                <div class="panel">
+								<div class="panel-heading">
+                                    <h3 class="panel-title">Data Siswa</h3>
+                                        <div class="right">
+                                            <button type="button" class="btn" data-toggle="modal" data-target="#exampleModal"><i class="lnr lnr-plus-circle"></i></button>
+                                        </div>
+								</div>
+								<div class="panel-body">
+									<table class="table table-hover">
+										<thead>
+											<tr>
+                                                <th>NAMA DEPAN</th>
+                                                <th>NAMA BELAKANG</th>
+                                                <th>JENIS KELAMIN</th>
+                                                <th>AGAMA</th>
+                                                <th>ALAMAT</th>
+                                                <th>AKSI</th>
+											</tr>
+										</thead>
+										<tbody>
+                                                @foreach($data_siswa as $siswa)
+                                                <tr>
+                                                    <td>{{$siswa->nama_depan}}</td>
+                                                    <td>{{$siswa->nama_belakang}}</td>
+                                                    <td>{{$siswa->jenis_kelamin}}</td>
+                                                    <td>{{$siswa->agama}}</td>
+                                                    <td>{{$siswa->alamat}}</td>
+                                                    <td>
+                                                        <a href="/siswa/{{$siswa->id}}/edit" class="btn btn-warning btn-sm">Edit</a>
+                                                        <a href="/siswa/{{$siswa->id}}/delete" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure to delete?')">Delete</a>
+                                                    </td>
+                                                </tr>
+                                                @endforeach
+										</tbody>
+									</table>
+								</div>
+							</div>
                 </div>
-                <div class="col-6">
-                    <button type="button" class="btn btn-primary btn-sm float-right" data-toggle="modal" data-target="#exampleModal">
-                    Tambah data siswa
-                    </button>
-                </div>
-            
-                <table class="table table-hover">
-                    <tr>
-                        <th>NAMA DEPAN</th>
-                        <th>NAMA BELAKANG</th>
-                        <th>JENIS KELAMIN</th>
-                        <th>AGAMA</th>
-                        <th>ALAMAT</th>
-                        <th>AKSI</th>
-                    </tr>
-                    @foreach($data_siswa as $siswa)
-                    <tr>
-                        <td>{{$siswa->nama_depan}}</td>
-                        <td>{{$siswa->nama_belakang}}</td>
-                        <td>{{$siswa->jenis_kelamin}}</td>
-                        <td>{{$siswa->agama}}</td>
-                        <td>{{$siswa->alamat}}</td>
-                        <td>
-                            <a href="/siswa/{{$siswa->id}}/edit" class="btn btn-warning btn-sm">Edit</a>
-                            <a href="/siswa/{{$siswa->id}}/delete" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure to delete?')">Delete</a>
-                        </td>
-                    </tr>
-                    @endforeach
-                </table>
             </div>
         </div>
-
-                <!-- Modal -->
-                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    </div>
+</div>
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div class="modal-dialog" role="document">
                         <div class="modal-content">
                         <div class="modal-header">
@@ -94,5 +100,4 @@
                         </div>
                             
                     </div>
-@endsection
-
+@stop
